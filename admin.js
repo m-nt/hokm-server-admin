d13c8ba3ccab4903597036d8839da2c7480baf2a
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2021-12-26 16:48:05 +0330

    update

--- admin.js
+++ admin.js
@@ -13,13 +13,10 @@ const PORT = process.env.PORT || 9090;
 const URL = require("./conf.json").MongoURL;
 const Options = require("./conf.json").MongoOpt;
 //Resources
-const VIP = require("./models/vipticket");
-const DEL_VIP = require("./models/deletedvip");
-const FriendListModel = require("./models/friendlist");
-const DeletedFriendListModel = require("./models/deletedfrndlist");
 const User = require("./models/User");
-const Report = require("./models/reports");
 AdminJS.registerAdapter(AdminJSMongoose);
+process.env.SESSION_SECRET = "secret";
+
 mongose
   .connect(URL, Options)
   .then((db) => {
@@ -28,34 +25,28 @@ mongose
       databases: [db],
       rootPath: "/admin",
     });
-    const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-      authenticate: async (Username, Password) => {
-        const user = await User.findOne({ Username });
-        if (user) {
-          const matched = await bcrypt.compare(Password, user.Password);
-          if (matched && user.role == "admin") {
-            return user;
+    const router = AdminJSExpress.buildAuthenticatedRouter(
+      adminJs,
+      {
+        authenticate: async (Username, Password) => {
+          const user = await User.findOne({ Username });
+          if (user) {
+            const matched = await bcrypt.compare(Password, user.Password);
+            if (matched && user.role == "admin") {
+              return user;
+            }
           }
-        }
-        return false;
-      },
-      cookiePassword: "some-secret-password-used-to-secure-cookie",
-    });
-    app.use(adminJs.options.rootPath, router);
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json({ extended: false }));
-    //Express session
-    app.use(
-      session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-          maxAge: 33333333333,
+          return false;
         },
-      })
+        cookiePassword: "some-secret-password-used-to-secure-cookie",
+      },
+      null,
+      {
+        resave: false,
+        saveUninitialized: true,
+      }
     );
-
+    app.use(adminJs.options.rootPath, router);
     console.log(`mongoose conected to Data Base...`);
   })
   .catch((err) => console.log(err));
